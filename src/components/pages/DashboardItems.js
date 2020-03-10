9edd8c3fac00528ae76af9352156d857b15aa1cf
mutesasira <katxalbert@gmail.com>
2020-03-10 20:49:03 +0300

    add checkbox color props

--- src/components/pages/DashboardItems.js
+++ src/components/pages/DashboardItems.js
@@ -9,6 +9,7 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import ChartIcon from '@material-ui/icons/InsertChart';
 import MapIcon from '@material-ui/icons/Public';
@@ -51,10 +52,36 @@ const useStyles = makeStyles(theme => ({
     maxHeight: 300, 
     overflow: 'auto'
   },
+  icon: {
+    borderRadius: 2,
+    width: 16,
+    height: 16,
+    boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    '$root.Mui-focusVisible &': {
+      outline: '2px auto rgba(19,124,189,.6)',
+      outlineOffset: 2,
+    },
+  },
+  checkedIcon: {
+    backgroundColor: '#137cbd',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+    '&:before': {
+      display: 'block',
+      width: 16,
+      height: 16,
+      backgroundImage:
+        "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+        " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+        "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
+      content: '""',
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#106ba3',
+    },
+  },
 }));
-const style = {
-  backgroundColor:"red",
-};
+
 const theme = createMuiTheme({
   overrides: {
     '.Mui-checked': {
@@ -89,7 +116,7 @@ const getItemIcon = type => {
       return <NotInterestedIcon />;
   }
 }
-export const DashboardItems = observer(() => {
+export const DashboardItems = observer((props) => {
   const { currentSetting } = useMst();
 
   const selectedDashboards = currentSetting.dashboards.filter(dash => {
@@ -159,14 +186,15 @@ export const DashboardItems = observer(() => {
                     <ListItemIcon>
                       <Checkbox
                         edge="start"
-                        className="colorRed"
+                        color='default'
+                        checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
                         checked={dashboardItem.selected}
                         onChange={dashboardItem.handleChange}
                         tabIndex={-1}
                         disableRipple
-                        inputProps={{
-                          'aria-labelledby': dsbId,
-                        }}
+                        icon={<span className={classes.icon} />}
+                        inputProps={{ 'aria-label': 'decorative checkbox' }}
+                        {...props}
                       />
                     </ListItemIcon>
                     <ListItemText
