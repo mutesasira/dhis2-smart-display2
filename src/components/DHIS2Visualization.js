c0dae500c01cecd9cbff9b6bccb7e686c3c0e34a
mutesasira <katxalbert@gmail.com>
2020-03-12 14:52:55 +0300

    make scrollbar work and adjust display cards

--- src/components/DHIS2Visualization.js
+++ src/components/DHIS2Visualization.js
@@ -9,8 +9,8 @@ export const DHIS2Visualization = observer(({ item, height, width }) => {
   let style = {
     height: height || '75vh',
     padding: '0 4px 4px',
-    overflow: 'auto',
-    maxHeight: 300,
+    overflow: 'none',
+    maxHeight: 140,
 
   };
 
