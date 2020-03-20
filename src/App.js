bf3532e77388a4f5b875d280ba4a1176e51c0465
carapai <olupotcharles@gmail.com>
2020-03-20 12:22:29 +0300

    Added router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDataEngine } from '@dhis2/app-runtime'
 import { DataProvider } from '@dhis2/app-runtime'
 import { Store } from './Store';
@@ -8,21 +8,35 @@ import 'antd/dist/antd.css';
 import './styles/tailwind.css';
 import { LandingPage } from './components/LandingPage';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 const MyApp = () => {
   const engine = useDataEngine();
   const { baseUrl, apiVersion } = useConfig();
   const rootStore = new Store(engine, baseUrl, apiVersion);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     rootStore.setD2().then(() => {
-      rootStore.fetchPresentations();
+      rootStore.fetchPresentations().then(() => {
+        setLoading(false);
+      });
     })
   }, [rootStore]);
 
   return <Provider value={rootStore}>
     <DataProvider>
-      <LandingPage />
+      <Router basename={window.location.pathname}>
+        <Switch>
+          <Route path="">
+            {loading ? <div>Loading</div> : <LandingPage />}
+          </Route>
+        </Switch>
+      </Router>
     </DataProvider>
   </Provider>
 }
